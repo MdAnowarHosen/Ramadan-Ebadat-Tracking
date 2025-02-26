d649d73c4654a7f7fe37ddb8300256953059c092
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-02-26 17:29:48 +0600

    Update TrackController.php

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
+use function Laravel\Prompts\error;
+
 class TrackController extends Controller
 {
     public function updateTask(Request $request, Task $task)
     {
         $action = auth()->user()->tasks()->toggle($task->id);
-
     }
 }
