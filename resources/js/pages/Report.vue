55effacc0227e162ecc8d6af058732a57995117d
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-02-28 15:10:58 +0600

    card design

--- resources/js/pages/Report.vue
+++ resources/js/pages/Report.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Report</h1>
+    <div class="grid grid-cols-2 gap-4 md:grid-cols-3 ">
+        <DashCard header="ফরজ স্বলাত" content="আদায় হয়েছে" description="৫ ওয়াক্ত" />
+        <DashCard header="ফরজ স্বলাত" content="আদায় হয়েছে" description="৫ ওয়াক্ত" />
+        <DashCard header="ফরজ স্বলাত" content="আদায় হয়েছে" description="৫ ওয়াক্ত" />
+        <DashCard header="ফরজ স্বলাত" content="আদায় হয়েছে" description="৫ ওয়াক্ত" />
+        <DashCard header="ফরজ স্বলাত" content="আদায় হয়েছে" description="৫ ওয়াক্ত" />
+        <DashCard header="ফরজ স্বলাত" content="আদায় হয়েছে" description="৫ ওয়াক্ত" />
+
+    </div>
 </template>
+
+<script lang="ts" setup>
+import DashCard from '../components/custom/DashCard.vue';
+
+
+</script>
