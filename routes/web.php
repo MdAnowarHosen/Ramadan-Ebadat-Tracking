ac0ebb68f79e009929cc17ef498fbab8f3b2744c
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-02-28 12:06:18 +0600

    report

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\TrackController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::controller(TrackController::class)->middleware(['auth', 'date'])->prefix('track')->name('track.')->group(function () {
-    Route::post('task/update/{task}', 'updateTask')->name('task.update');
-    Route::post('salat/update/{salat}', 'updateSalat')->name('salat.update');
-    Route::post('salat/sunnah_rakat/update/{salat}', 'updateSunnahSalat')->name('salat.sunnah.update');
-    Route::post('quran/update', 'updateQuran')->name('quran.update');
+Route::middleware(['auth', 'date'])->group(function () {
+    Route::controller(TrackController::class)->prefix('track')->name('track.')->group(function () {
+        Route::post('task/update/{task}', 'updateTask')->name('task.update');
+        Route::post('salat/update/{salat}', 'updateSalat')->name('salat.update');
+        Route::post('salat/sunnah_rakat/update/{salat}', 'updateSunnahSalat')->name('salat.sunnah.update');
+        Route::post('quran/update', 'updateQuran')->name('quran.update');
+    });
+
+    Route::controller(ReportController::class)->prefix('report')->name('report.')->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
 });
+
 
 Route::middleware(['auth', 'admin', 'verified'])->group(function () {
     Route::get('dashboard', function () {
