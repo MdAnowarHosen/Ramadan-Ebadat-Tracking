73fcb96ccba50410ebfc40299eec7b94a5285e3b
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-03-04 11:30:54 +0600

    salat

--- app/Http/Controllers/Admin/AdminSalatController.php
+++ app/Http/Controllers/Admin/AdminSalatController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Salat;
 use Illuminate\Http\Request;
 
 class AdminSalatController extends Controller
 {
     public function index(Request $request)
     {
-        dd('ok');
+        $data = Salat::all();
+        return inertia('Admin/Salat/Salat', ['data' => $data]);
     }
 }
---
+++ resources/js/pages/Admin/Salat/Salat.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Salat',
+        href: '/admin/salat',
+    },
+];
+
+defineProps<{
+    data?: object;
+}>();
+
+</script>
+
+<template>
+    <Head title="Salat" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+  <Table>
+    <TableCaption>A list of your recent invoices.</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="w-[100px]">
+          Invoice
+        </TableHead>
+        <TableHead>Status</TableHead>
+        <TableHead>Method</TableHead>
+        <TableHead class="text-right">
+          Amount
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell class="font-medium">
+          INV001
+        </TableCell>
+        <TableCell>Paid</TableCell>
+        <TableCell>Credit Card</TableCell>
+        <TableCell class="text-right">
+          $250.00
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+  </AppLayout>
+</template>
