13576339a0427109241da76d9075d22a843ba969
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-03-04 13:01:02 +0600

    Update SalatCreate.vue

--- resources/js/pages/Admin/Salat/SalatCreate.vue
+++ resources/js/pages/Admin/Salat/SalatCreate.vue
@@ -4,8 +4,33 @@ import { Head } from '@inertiajs/vue3';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 
-import { Button } from "@/components/ui/button"
 import { Link } from '@inertiajs/vue3';
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50),
+}))
+
+const form = useForm({
+  validationSchema: formSchema,
+})
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log('Form submitted!', values)
+})
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -23,6 +48,22 @@ defineProps<{
 <template>
     <Head title="স্বলাত যুক্ত করুন" />
     <AppLayout :breadcrumbs="breadcrumbs">
-
+        <form @submit="onSubmit">
+            <FormField v-slot="{ componentField }" name="username">
+            <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                <Input type="text" placeholder="shadcn" v-bind="componentField" />
+                </FormControl>
+                <FormDescription>
+                This is your public display name.
+                </FormDescription>
+                <FormMessage />
+            </FormItem>
+            </FormField>
+            <Button type="submit">
+            Submit
+            </Button>
+        </form>
   </AppLayout>
 </template>
