170905b38002a642101b788c33ac1fe23b60ab53
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-03-06 10:01:20 +0600

    cache salat data

--- app/Classes/PryingTime.php
+++ app/Classes/PryingTime.php
@@ -5,6 +5,8 @@ namespace App\Classes;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Http;
 use Rakibhstu\Banglanumber\NumberToBangla;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Auth;
 
 class PryingTime
 {
@@ -20,24 +22,27 @@ class PryingTime
      */
     public static function get(string $latitude, string $longitude, ?string $date = null): object
     {
-        $date = $date ?? now()->format('Y-m-d');
+        $cacheKey = "prayer_times_{Auth::user()->id}";
+        return Cache::remember($cacheKey, now()->addHours(1), function () use ($latitude, $longitude, $date) {
+            $date = $date ?? now()->format('Y-m-d');
 
-        $response = Http::retry(3, 100)->withQueryParameters([
-            'latitude' => $latitude,
-            'longitude' => $longitude,
-            'method' => '1', // University of Islamic Sciences, Karachi
-            'timezonestring' => 'Asia/Dhaka',
-            'iso8601' => 'true',
-        ])->get('https://api.aladhan.com/v1/timings/' . $date)->json();
+            $response = Http::retry(3, 100)->withQueryParameters([
+                'latitude' => $latitude,
+                'longitude' => $longitude,
+                'method' => '1', // University of Islamic Sciences, Karachi
+                'timezonestring' => 'Asia/Dhaka',
+                'iso8601' => 'true',
+            ])->get('https://api.aladhan.com/v1/timings/' . $date)->json();
 
 
-        $numto = new NumberToBangla();
-        $timing = collect($response['data']['timings']) ?? [];
+            $numto = new NumberToBangla();
+            $timing = collect($response['data']['timings']) ?? [];
 
-        // Convert each time to 12-hour format
-        return $timing->each(function ($time, $key) use ($timing, $numto) {
-            $time = Carbon::parse($time);
-            $timing[$key] = $numto->bnNum($time->format('g')) . ':' . $numto->bnNum($time->format('i'));
+            // Convert each time to 12-hour format
+            return $timing->each(function ($time, $key) use ($timing, $numto) {
+                $time = Carbon::parse($time);
+                $timing[$key] = $numto->bnNum($time->format('g')) . ':' . $numto->bnNum($time->format('i'));
+            });
         });
     }
 }
